4b1a390e38541417ee8135918710b09664175897
Baimenovbn <baibn@mail.ru>
2022-12-23 00:25:11 +0600

    feat: finish task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = number / 1000;
+        int second = (number / 100) % 10;
+        int third = number / 10 % 10;
+        int last = number % 10;
+
+        System.out.println(last + third + second + first);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float coefficient = 9 / (float) 5;
+
+        System.out.println(temperatureCelsius * coefficient + 32);
     }
 }
